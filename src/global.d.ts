f395c618ce546f4e1c6521be797006f594921891
kota65535 <kota65535@gmail.com>
2018-02-10 19:06:59 +0900

    unneeded file

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,6 +3,17 @@ import PaperScope = paper.PaperScope;
 declare module "*.json"
 declare module '*.jpg'
 
+// for style loader
+declare module '*.css' {
+  const styles: any;
+  export default styles;
+}
+
+declare module '*.svg' {
+  const data: any;
+  export default data;
+}
+
 declare var paperScope: PaperScope|any
 declare var RAIL_COMPONENTS: any
 

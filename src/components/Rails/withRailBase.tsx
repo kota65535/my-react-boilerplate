4847395bc7436c5c6d128b737b94dbe30683240c
kota65535 <kota65535@gmail.com>
2018-03-21 19:16:26 +0900

    arranged

--- src/components/Rails/withRailBase.tsx
+++ src/components/Rails/withRailBase.tsx
@@ -91,6 +91,11 @@ export default function withRailBase(WrappedComponent: React.ComponentClass<Rail
     get railPart() { return this.rail.railPart }
     get joints() { return this.rail.joints }
 
+    // TODO: これでOK?
+    // shouldComponentUpdate() {
+    //   return false
+    // }
+
     /**
      * レールパーツを左クリックしたら、レールの選択状態をトグルする。
      * @param {MouseEvent} e
@@ -110,63 +115,12 @@ export default function withRailBase(WrappedComponent: React.ComponentClass<Rail
     }
 
     /**
-     * ジョイントを右クリックしたら、仮レールが接続するジョイントを変更する
-     * @param {number} jointId
-     * @param {MouseEvent} e
-     */
-    onJointRightClick = (jointId: number, e: MouseEvent) => {
-      const temporaryRail = getTemporaryRailComponent()
-      const numJoints = temporaryRail.props.numJoints
-      const stride = temporaryRail.props.pivotJointChangingStride
-
-      // 仮レールのPivotJointを加算する
-      let temporaryPivotJointIndex = (this.props.temporaryPivotJointIndex + stride) % numJoints
-      this.props.setTemporaryPivotJoint(temporaryPivotJointIndex)
-
-      // 新たに仮レールの近傍ジョイントを探索して検出状態にする
-      this.undetectCloseJoints(true)
-      this.detectCloseJoints(temporaryRail)
-      LOGGER.info(`close joints: ${this.reasonablyCloseJoints}`)
-      return true
-    }
-
-    // TODO: これでOK?
-    // shouldComponentUpdate() {
-    //   return false
-    // }
-
-    /**
-     * 仮レールが現在のレイヤーの他のレールに衝突しているかどうか調べる
-     * @param {RailData} railData
-     */
-    private temporaryRailIntersects() {
-      // 仮レールを構成するPathオブジェクト
-      const targetRailPaths = getTemporaryRailComponent().railPart.path.children
-      // 現在のレイヤーにおける各レールと仮レールが重なっていないか調べる
-      const result = getRailComponentsOfLayer(this.props.activeLayerId)
-        .filter(r => r.props.id !== this.props.id)
-        .map(r => r.railPart.path)
-        .map(group => {
-          // 両レールを構成するパス同士の組み合わせを作成し、重なりを調べる
-          const combinations = Combinatorics.cartesianProduct(group.children, targetRailPaths).toArray()
-          const result = combinations.map(cmb => cmb[0].intersects(cmb[1])).every(e => e)
-          LOGGER.debug(`Rail ${group.data.railId}: ${result}`)
-          return result
-        })
-        .some(e => e)   // 重なっているものが一つ以上あればtrue
-
-      // LOGGER.debug(`tempo intersected ${result}`)
-      return result
-    }
-
-
-    /**
      * ジョイントを左クリックしたら、仮レールの位置にレールを設置する
      * @param {number} jointId
      * @param {MouseEvent} e
      */
     onJointLeftClick = (jointId: number, e: MouseEvent) => {
-      // 仮レールがこのレイヤーの他のレールと重なっていないか調べる
+      // 仮レールがこのレイヤーの他のレールと重なっていたら、何もせずに返る
       const intersects = this.temporaryRailIntersects()
       if (intersects) {
         LOGGER.info("Rail intersects.")
@@ -219,6 +173,27 @@ export default function withRailBase(WrappedComponent: React.ComponentClass<Rail
     }
 
     /**
+     * ジョイントを右クリックしたら、仮レールが接続するジョイントを変更する
+     * @param {number} jointId
+     * @param {MouseEvent} e
+     */
+    onJointRightClick = (jointId: number, e: MouseEvent) => {
+      const temporaryRail = getTemporaryRailComponent()
+      const numJoints = temporaryRail.props.numJoints
+      const stride = temporaryRail.props.pivotJointChangingStride
+
+      // 仮レールのPivotJointを加算する
+      let temporaryPivotJointIndex = (this.props.temporaryPivotJointIndex + stride) % numJoints
+      this.props.setTemporaryPivotJoint(temporaryPivotJointIndex)
+
+      // 新たに仮レールの近傍ジョイントを探索して検出状態にする
+      this.undetectCloseJoints(true)
+      this.detectCloseJoints(temporaryRail)
+      LOGGER.info(`close joints: ${this.reasonablyCloseJoints}`)
+      return true
+    }
+
+    /**
      * ジョイント上でマウスが動いた場合
      * 仮レールのジョイントが他のジョイントに十分近い場合、そのジョイントの検出状態を変更する
      * @param {number} jointId
@@ -237,53 +212,6 @@ export default function withRailBase(WrappedComponent: React.ComponentClass<Rail
       // 仮レールの近傍にあるジョイントを検出中状態に変更する
       this.detectCloseJoints(temporaryRail)
     }
-
-    /**
-     * 指定のレールの近傍にあるジョイントを検出中状態に変更する
-     * @param {RailBase<any, any>} rail
-     */
-    detectCloseJoints(rail: RailBase<any, any>) {
-      let ret = []
-      // 自分以外の全てのレールに対して実行する
-      Object.keys(window.RAIL_COMPONENTS)
-        .filter(id => id !== rail.props.id.toString())  // IDが文字列での比較になることに注意
-        .map(k => window.RAIL_COMPONENTS[k])
-        .forEach(target => {
-          // 対象レールと指定レールのジョイントの組み合わせ
-          const combinations = Combinatorics.cartesianProduct(target.joints, rail.joints).toArray()
-          combinations.forEach(cmb => {
-            // ジョイントが十分近ければリストに加える
-            const isClose = pointsEqual(cmb[0].position, cmb[1].position, 0.1)
-            if (isClose) {
-              ret.push(cmb)
-            }
-          })
-        })
-
-      // 検出中状態にする
-      ret.forEach(cmb => cmb[0].part.setState({
-        detectionState: DetectionState.DETECTING,
-        detectionPartVisible: true
-      }))
-      this.reasonablyCloseJoints = ret
-    }
-
-    /**
-     * 全ての近傍ジョイントを非検出状態に戻す
-     * @param {RailBase<any, any>} rail
-     */
-    undetectCloseJoints(doNullify: boolean) {
-      if (this.reasonablyCloseJoints != null) {
-        this.reasonablyCloseJoints.forEach(cmb => cmb[0].part.setState({
-          detectionState: DetectionState.BEFORE_DETECT,
-          detectionPartVisible: true
-        }))
-        if (doNullify) {
-          this.reasonablyCloseJoints = null
-        }
-      }
-    }
-
 
     /**
      * ジョイントにマウスが乗ったら、仮レールを表示する
@@ -314,14 +242,6 @@ export default function withRailBase(WrappedComponent: React.ComponentClass<Rail
         pivotJointIndex: pivotJointIndex,
         enableJoints: false
       })
-
-    }
-
-    refInstance = (instance: RailBase<any, any>) => {
-      this.rail = instance
-      if (this.props.refInstance) {
-        this.props.refInstance(instance)
-      }
     }
 
     /**
@@ -333,6 +253,85 @@ export default function withRailBase(WrappedComponent: React.ComponentClass<Rail
       this.props.setTemporaryItem(null)
       this.undetectCloseJoints(true)
     }
+
+    /**
+     * RailComponentクラスを取得するためのコールバック
+     * @param {RailBase<any, any>} instance
+     */
+    refInstance = (instance: RailBase<any, any>) => {
+      this.rail = instance
+      if (this.props.refInstance) {
+        this.props.refInstance(instance)
+      }
+    }
+
+    /**
+     * 指定のレールの近傍にあるジョイントを検出中状態に変更する
+     * @param {RailBase<any, any>} rail
+     */
+    private detectCloseJoints(rail: RailBase<any, any>) {
+      let ret = []
+      // 自分以外の全てのレールに対して実行する
+      Object.keys(window.RAIL_COMPONENTS)
+        .filter(id => id !== rail.props.id.toString())  // IDが文字列での比較になることに注意
+        .map(k => window.RAIL_COMPONENTS[k])
+        .forEach(target => {
+          // 対象レールと指定レールのジョイントの組み合わせ
+          const combinations = Combinatorics.cartesianProduct(target.joints, rail.joints).toArray()
+          combinations.forEach(cmb => {
+            // ジョイントが十分近ければリストに加える
+            const isClose = pointsEqual(cmb[0].position, cmb[1].position, 0.1)
+            if (isClose) {
+              ret.push(cmb)
+            }
+          })
+        })
+
+      // 検出中状態にする
+      ret.forEach(cmb => cmb[0].part.setState({
+        detectionState: DetectionState.DETECTING,
+        detectionPartVisible: true
+      }))
+      this.reasonablyCloseJoints = ret
+    }
+
+    /**
+     * 全ての近傍ジョイントを非検出状態に戻す
+     */
+    private undetectCloseJoints(doNullify: boolean) {
+      if (this.reasonablyCloseJoints != null) {
+        this.reasonablyCloseJoints.forEach(cmb => cmb[0].part.setState({
+          detectionState: DetectionState.BEFORE_DETECT,
+          detectionPartVisible: true
+        }))
+        if (doNullify) {
+          this.reasonablyCloseJoints = null
+        }
+      }
+    }
+
+    /**
+     * 仮レールが現在のレイヤーの他のレールに衝突しているかどうか調べる
+     */
+    private temporaryRailIntersects() {
+      // 仮レールを構成するPathオブジェクト
+      const targetRailPaths = getTemporaryRailComponent().railPart.path.children
+      // 現在のレイヤーにおける各レールと仮レールが重なっていないか調べる
+      const result = getRailComponentsOfLayer(this.props.activeLayerId)
+        .filter(r => r.props.id !== this.props.id)
+        .map(r => r.railPart.path)
+        .map(group => {
+          // 両レールを構成するパス同士の組み合わせを作成し、重なりを調べる
+          const combinations = Combinatorics.cartesianProduct(group.children, targetRailPaths).toArray()
+          const result = combinations.map(cmb => cmb[0].intersects(cmb[1])).every(e => e)
+          LOGGER.debug(`Rail ${group.data.railId}: ${result}`)
+          return result
+        })
+        .some(e => e)   // 重なっているものが一つ以上あればtrue
+
+      return result
+    }
+
 
     render() {
       return (

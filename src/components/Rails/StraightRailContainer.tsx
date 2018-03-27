53f91e26631e3f77fd3a1f615f129123e4807fe6
kota65535 <kota65535@gmail.com>
2018-03-28 02:36:36 +0900

    hoc

--- src/components/Rails/StraightRailContainer.tsx
+++ src/components/Rails/StraightRailContainer.tsx
@@ -1,14 +1,5 @@
-import * as React from "react";
-import {Rectangle} from "react-paper-bindings";
-import {compose, defaultProps} from "recompose";
-import {default as withBuilder} from "components/hoc/withBuilder";
-import StraightRail, {StraightRailProps} from "components/Rails/StraightRail";
-import withRailBase from "components/Rails/withRailBase";
+import StraightRail from "components/Rails/StraightRail";
+import {railHocs} from "components/Rails/hoc";
 
-
-export default compose<StraightRailProps, StraightRailProps>(
-  defaultProps(StraightRail.defaultProps),
-  withBuilder,
-  withRailBase
-)(StraightRail)
+export default railHocs(StraightRail)
 
